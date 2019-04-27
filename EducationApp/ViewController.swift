ca3d395947ef8fe5ec3b3082362af8bb9b48d561
mukhammadjonAskarov <asqarov.muhammadjon@mail.ru>
2019-04-27 11:30:22 +0500

    Initial Commit

---
+++ EducationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EducationApp
+//
+//  Created by Mukhammadjon Askarov on 4/27/19.
+//  Copyright © 2019 Mukhammadjon Askarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
